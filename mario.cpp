a61224db406dc235e6cc8fd1f0d1ba107add9304
Mariia Boirchuk <mboiar2004@gmail.com>
2020-05-03 17:59:05 +0300

    Creating program

--- mario.cpp
+++ mario.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#define NUMBER_TEN 10
 using namespace std;
 
 int main()
 {
+	int height, option;
+
+	cout << "Enter height pyramid. Max = " << NUMBER_TEN << endl;
+
+	while (true)
+	{
+		cin >> height;
+
+		if (!cin || height > NUMBER_TEN || height <= 0)
+		{
+			cout << "Incorrect input. Please re-enter: ";
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else
+			break;
+	}
+
+	cout << "Height = " << height << endl;
+	cout << "Enter option pyramid. Only 1 or 2\n";
+
+	while (true)
+	{
+		cin >> option;
+
+		if (option == 1 || option == 2)
+			break;
+		else
+			cout << "Incorrect input. Please re-enter: ";
+
+		cin.clear();
+		while (cin.get() != '\n');
+	}
+
+	cout << "Option = " << option << endl;
+	cout << "Your pyramid\n";
 
 	system("pause");
 	return 0;
